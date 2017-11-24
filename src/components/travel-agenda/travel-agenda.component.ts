09aaea537d9d9e700c905b925521453934c3ac7b
Xiaotian Cheng <xiaotiancheng@Xiaotians-MacBook-Air.local>
2017-11-24 23:08:03 +0800

    commit

--- src/components/travel-agenda/travel-agenda.component.ts
+++ src/components/travel-agenda/travel-agenda.component.ts
@@ -1,8 +1,9 @@
+import { dailyTrip } from '../../modules/store/entity/entity.schema';
 import { NgRedux } from '@angular-redux/store/lib/src/components/ng-redux';
 import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs/Rx';
 
-import { ITravelAgenda } from '../../modules/store/entity/travelAgenda/travelAgenda.model';
+import { IDailyTrip, ITravelAgenda } from '../../modules/store/entity/travelAgenda/travelAgenda.model';
 import { IAppState } from '../../modules/store/store.model';
 import { getSelectedTravelAgenda } from '../../modules/store/ui/travelAgenda/travelAgenda.selector';
 import { DragulaService } from '../../providers/dragula.service';
@@ -15,15 +16,13 @@ export class TravelAgendaComponent implements AfterViewInit,OnDestroy {
   //#region Private member
 
   private _unSubs : Array<Subscription> = new Array<Subscription>();
-  protected _selectedTravelAgenda$: Observable<ITravelAgenda>;
-  
   
   //#endregion
 
   //#region Protected member
-
+  protected selectedTravelAgenda$: Observable<ITravelAgenda>;
   protected travelAgenda : ITravelAgenda;
-  
+  protected selectedDailyTrip : IDailyTrip;
   //#endregion
 
   //#region Private property
@@ -36,16 +35,16 @@ export class TravelAgendaComponent implements AfterViewInit,OnDestroy {
 
   //#region Constructor
   constructor(private _store: NgRedux<IAppState>,private _dragulaService: DragulaService) {
-    this._selectedTravelAgenda$ = this._store.select<string>(['ui', 'travelAgenda','selectedId'])
+    this.selectedTravelAgenda$ = this._store.select<string>(['ui', 'travelAgenda','selectedId'])
       .map(getSelectedTravelAgenda(this._store));
   }
   //#endregion
 
   //#region Interface implementation
   ngAfterViewInit(): void {
-    this._unSubs.push(this._selectedTravelAgenda$.subscribe(travelAgenda => {
-      this.travelAgenda = travelAgenda;
-    }));
+    // this._unSubs.push(this.selectedTravelAgenda$.subscribe(travelAgenda => {
+    //   this.travelAgenda = travelAgenda;
+    // }));
   }
 
   ngOnDestroy(): void {
@@ -63,7 +62,9 @@ export class TravelAgendaComponent implements AfterViewInit,OnDestroy {
   //#endregion
 
   //#region Protected method
- 
+  protected dayClicked(dailyTrip : IDailyTrip) {
+    this.selectedDailyTrip = dailyTrip;
+  }
   //#endregion
 
   //#region Private method
